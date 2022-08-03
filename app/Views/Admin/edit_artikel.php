4d8c891f2a06fedd06321bd64fa8686e842ece7b
Ahmad Ilham <angilham2@gmail.com>
2022-08-03 12:20:23 +0700

    add modal

--- app/Views/Admin/edit_artikel.php
+++ app/Views/Admin/edit_artikel.php
@@ -1,79 +1,95 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-    <!-- Carousel -->
-    <link
-      rel="stylesheet"
-      href="./OwlCarousel2-2.3.4/dist/assets/owl.carousel.min.css"
-    />
-    <link
-      rel="stylesheet"
-      href="./OwlCarousel2-2.3.4/dist/assets/owl.theme.default.min.css"
-    />
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-    <!-- Bootstrap CSS -->
-    <link
-      href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
-      rel="stylesheet"
-      integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
-      crossorigin="anonymous"
-    />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <!-- Carousel -->
+  <link rel="stylesheet" href="./OwlCarousel2-2.3.4/dist/assets/owl.carousel.min.css" />
+  <link rel="stylesheet" href="./OwlCarousel2-2.3.4/dist/assets/owl.theme.default.min.css" />
 
-    <!-- My Css -->
-    <link rel="stylesheet" href="./assets/style.css" />
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
-    <title>Kejayaan</title>
-  </head>
-  <body>
-    <!-- navbar -->
-  
-    <!-- tutup navbar -->
+  <!-- My Css -->
+  <link rel="stylesheet" href="./assets/style.css" />
 
-    <!-- konten 1 -->
-    <div class="container" style="height: 414px">
-      <div class="row">
-        <div class="col-sm-2"></div>
-        <div class="col-sm-8 mt-5">
+  <title>Kejayaan</title>
+</head>
+
+<body>
+  <!-- navbar -->
+  <nav class="navbar navbar-expand-lg main-nav">
+    <div class="container">
+      <a class="navbar-brand" href="<?= base_url('');?>" style="color: black;"><img
+          src="./assets/images/logo_tanggamus-remove.png" alt="" width="35" height="40" /> Pekon Kejayaan</a>
+
+      <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse menu-nav" id="navbarNav">
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="admin">Beranda</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link btn btn-primary" href="<?= base_url('logout')?>">Keluar</a>
+          </li>
+          <!-- <li class="nav-item">
+              <a class="btn nav-button">Register</a>
+            </li> -->
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <!-- tutup navbar -->
+
+  <!-- konten 1 -->
+  <div class="container" style="height: 414px">
+    <div class="row">
+      <div class="col-sm-2"></div>
+      <div class="col-sm-8 mt-5">
         <form action="<?=base_url('update/'.$artikel["id"])?>" method="post" enctype="multipart/form-data">
 
           <div class="mb-1">
             <label for="exampleInputEmail1" class="form-label">Judul Artikel</label>
-            <input type="text" class="form-control" name="judul" id="exampleInputEmail1" aria-describedby="emailHelp" value="<?=$artikel["judul"]?>">
+            <input type="text" class="form-control" name="judul" id="exampleInputEmail1" aria-describedby="emailHelp"
+              value="<?=$artikel["judul"]?>">
           </div>
           <div class="mb-1">
             <label for="exampleInputEmail1" class="form-label">Slug</label>
-            <input type="text" class="form-control" name="slug" id="exampleInputEmail1" aria-describedby="emailHelp" value="<?=$artikel["slug"]?>">
+            <input type="text" class="form-control" name="slug" id="exampleInputEmail1" aria-describedby="emailHelp"
+              value="<?=$artikel["slug"]?>">
           </div>
           <div class="form-group mb-2">
             <label for="exampleFormControlTextarea1">Isi Berita</label>
-            <textarea class="form-control" name="isi" id="exampleFormControlTextarea1" rows="3" value=""><?=$artikel["isi"]?></textarea>
+            <textarea class="form-control" name="isi" id="exampleFormControlTextarea1" rows="3"
+              value=""><?=$artikel["isi"]?></textarea>
           </div>
-            <div class="custom-file mb-2">
-                <input type="file" name="gambar" class="custom-file-input" id="gambar">
-                <label class="custom-file-label" for="gambar">Pilih Gambar</label>
-            </div>
-          
-          
-          <button type="submit"class="btn btn-primary">UBAH</button>
-        </form></div>
-        <div class="col-sm-2"></div>
-      </div>
-    </div>
-    <!-- Konten 1 close -->
-   
-    <!-- footer -->
-  
-    <!-- footer -->
+          <div class="custom-file mb-2">
+            <input type="file" name="gambar" class="custom-file-input" id="gambar">
+            <label class="custom-file-label" for="gambar">Pilih Gambar</label>
+          </div>
 
-    <script
-      src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-      integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
-      crossorigin="anonymous"
-    ></script>
-  </body>
+
+          <button type="submit" class="btn btn-primary">UBAH</button>
+        </form>
+      </div>
+      <div class="col-sm-2"></div>
+    </div>
+  </div>
+  <!-- Konten 1 close -->
+
+  <!-- footer -->
+
+  <!-- footer -->
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+  </script>
+</body>
+
 </html>
--- app/Views/detailBerita.php
+++ app/Views/detailBerita.php
@@ -1,73 +1,60 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-    <!-- Carousel -->
-    <link
-      rel="stylesheet"
-      href="./OwlCarousel2-2.3.4/dist/assets/owl.carousel.min.css"
-    />
-    <link
-      rel="stylesheet"
-      href="./OwlCarousel2-2.3.4/dist/assets/owl.theme.default.min.css"
-    />
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-    <!-- Bootstrap CSS -->
-    <link
-      href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
-      rel="stylesheet"
-      integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
-      crossorigin="anonymous"
-    />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <!-- Carousel -->
+  <link rel="stylesheet" href="./OwlCarousel2-2.3.4/dist/assets/owl.carousel.min.css" />
+  <link rel="stylesheet" href="./OwlCarousel2-2.3.4/dist/assets/owl.theme.default.min.css" />
 
-    <!-- My Css -->
-    <link rel="stylesheet" href="./assets/style.css" />
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
-    <title>Kejayaan</title>
-  </head>
-  <body>
-    <!-- navbar -->
-    <nav class="navbar navbar-expand-lg main-nav">
-      <div class="container">
-        <a class="navbar-brand" href="#" style="color: black;"
-          ><img
-            src="./assets/images/logo_tanggamus-remove.png"
-            alt=""
-            width="35"
-            height="40"
-        /> Pekon Kejayaan</a>
-        
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse menu-nav" id="navbarNav">
-          <ul class="navbar-nav ms-auto">
-            <li class="nav-item">
-              <a class="nav-link" href="./index.html">Beranda</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="./profil.html">Profil</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="./Berita.html">Berita</a>
-            </li>
-            <li class="nav-item">
-              <a class="btn btn-primary" href="./login.html">Login</a>
-            </li>
-            <!-- <li class="nav-item">
+  <!-- My Css -->
+  <link rel="stylesheet" href="./assets/style.css" />
+
+  <title>Kejayaan</title>
+</head>
+
+<body>
+  <!-- navbar -->
+  <nav class="navbar navbar-expand-lg main-nav">
+    <div class="container">
+      <a class="navbar-brand" href="#" style="color: black;"><img src="./assets/images/logo_tanggamus-remove.png" alt=""
+          width="35" height="40" /> Pekon Kejayaan</a>
+
+      <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse menu-nav" id="navbarNav">
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="./index.html">Beranda</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="./profil.html">Profil</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="./Berita.html">Berita</a>
+          </li>
+          <li class="nav-item">
+            <a class="btn btn-primary" href="./login.html">Login</a>
+          </li>
+          <!-- <li class="nav-item">
               <a class="btn nav-button">Register</a>
             </li> -->
-          </ul>
-        </div>
+        </ul>
       </div>
-    </nav>
-    <!-- tutup navbar -->
+    </div>
+  </nav>
+  <!-- tutup navbar -->
 
-    <!-- konten 1 -->
-    <!-- <div class="container-fluid jumbotron">
+  <!-- konten 1 -->
+  <!-- <div class="container-fluid jumbotron">
       <div class="row main-content-1 ">
         <div class="col-sm-6 mt-5 text-center">
           <h3>Visi</h3>
@@ -83,26 +70,27 @@
         </div>
       </div>
     </div> -->
-    <!-- tutup konten 1 -->
+  <!-- tutup konten 1 -->
 
-    <!-- Artikel  -->
-    <div class="container mx-auto mt-5 mb-5 news" style="border: 2px solid rgb(241,243,255);">
-      <h3 class="text-center" style="border-bottom: 3px solid #f1f3ff;"><?= $artikel['judul'];?></h3>
-      <div class="row">
-        <div class="col-2"></div>
-        <div class="col-8 text-center">
-          <img src="./assets/images/<?= $artikel['gambar'];?>" alt="" class="mt-5" style="border: 20px solid #f1f3ff; border-radius: 20px;">
-          <p style="text-align: justify;" class="mt-4"><?= $artikel['isi'];?></p>
-        </div>
-        <div class="col-2"></div>
-    </div>    
-     
-</div>
+  <!-- Artikel  -->
+  <div class="container mx-auto mt-5 mb-5 news" style="border: 2px solid rgb(241,243,255);">
+    <h3 class="text-center" style="border-bottom: 3px solid #f1f3ff;"><?= $artikel['judul'];?></h3>
+    <div class="row">
+      <div class="col-2"></div>
+      <div class="col-8 text-center">
+        <img src="./assets/images/<?= $artikel['gambar'];?>" alt="" class="mt-5"
+          style="border: 20px solid #f1f3ff; border-radius: 20px;">
+        <p style="text-align: justify;" class="mt-4"><?= $artikel['isi'];?></p>
+      </div>
+      <div class="col-2"></div>
+    </div>
+
   </div>
-    <!-- Artikel Tutup -->
+  </div>
+  <!-- Artikel Tutup -->
 
-    <!-- konten 2 -->
-    <!-- <div class="container main-content-2 mt-5 mb-5" id="OurServices">
+  <!-- konten 2 -->
+  <!-- <div class="container main-content-2 mt-5 mb-5" id="OurServices">
       <div class="">
         <div class="row">
           <h2 class="text-center">Lokasi Pekon Kejayaan</h2>
@@ -113,79 +101,55 @@
         </div>
       </div>
     </div> -->
-    <!-- tutup konten 2 -->
+  <!-- tutup konten 2 -->
 
-    <!-- Card -->
-    <div class="container-fluid">
-       
-    </div>
-    <!-- Card Close -->
+  <!-- Card -->
+  <div class="container-fluid">
 
-    <!-- footer -->
-    <div class="container-fluid footer">
-      <div class="row">
-        <div class="col-sm-3">
-          <p>Jalan Suroyo No. 161 Mayangan Kota Probolonggo 672000</p>
-          <p>binarcarrental@gmail.com</p>
-          <p>081-233-334-808</p>
-        </div>
-        <div class="col-sm-3">
-          <p>Our services</p>
-          <p>Why Us</p>
-          <p>timonial</p>
-          <p>FAQ</p>
-        </div>
-        <div class="col-sm-3">
-          <p>Connect with us</p>
-          <ul class="ps-0">
-            <li>
-              <i class="fa fa-facebook"></i>
-            </li>
-            <li>  
-              <img
-                src="./assets/images/icon_instagram.png"
-                width="32px"
-                height="32px"
-                alt=""
-              />
-            </li>
-            <li>
-              <img
-                src="./assets/images/icon_twitter.png"
-                width="32px"
-                height="32px"
-                alt=""
-              />
-            </li>
-            <li>
-              <img
-                src="./assets/images/icon_mail.png"
-                width="32px"
-                height="32px"
-                alt=""
-              />
-            </li>
-            <li>
-              <img
-                src="./assets/images/icon_twitch.png"
-                width="32px"
-                height="32px"
-                alt=""
-              />
-            </li>
-          </ul>
-        </div>
-        <div class="col-3">
-          <p>Copyright Binar 2022</p>
-          <img
-            src="./assets/images/Rectangle 74.png"
-            width="100px"
-            height="34px"
-            alt=""
-          />
-        </div>
+  </div>
+  <!-- Card Close -->
+
+  <!-- footer -->
+  <div class="container-fluid footer">
+    <div class="row">
+      <div class="col-sm-3">
+        <p>Jalan Suroyo No. 161 Mayangan Kota Probolonggo 672000</p>
+        <p>binarcarrental@gmail.com</p>
+        <p>081-233-334-808</p>
+      </div>
+      <div class="col-sm-3">
+        <p>Our services</p>
+        <p>Why Us</p>
+        <p>timonial</p>
+        <p>FAQ</p>
+      </div>
+      <div class="col-sm-3">
+        <p>Connect with us</p>
+        <ul class="ps-0">
+          <li>
+            <i class="fa fa-facebook"></i>
+          </li>
+          <li>
+            <img src="./assets/images/icon_instagram.png" width="32px" height="32px" alt="" />
+          </li>
+          <li>
+            <img src="./assets/images/icon_twitter.png" width="32px" height="32px" alt="" />
+          </li>
+          <li>
+            <img src="./assets/images/icon_mail.png" width="32px" height="32px" alt="" />
+          </li>
+          <li>
+            <img src="./assets/images/icon_twitch.png" width="32px" height="32px" alt="" />
+          </li>
+        </ul>
+      </div>
+      <div class="col-3">
+        <p>Copyright Binar 2022</p>
+        <img src="./assets/images/Rectangle 74.png" width="100px" height="34px" alt="" />
       </div>
     </div>
-    <!-- footer -->
-  </body>
+  </div>
+  <!-- footer -->
+</body>
+
 </html>
